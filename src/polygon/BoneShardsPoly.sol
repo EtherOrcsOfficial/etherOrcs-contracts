ae3c6adc1ec94806a5ab9d60262fdb068f6fea21
naomsa <omar@na2.com.br>
2021-12-10 17:18:16 -0300

    changed contract names to avoid duplication

--- src/polygon/BoneShardsPoly.sol
+++ src/polygon/BoneShardsPoly.sol
@@ -3,8 +3,8 @@ pragma solidity 0.8.7;
 
 import "../ERC20.sol";
 
-contract BoneShards is ERC20 {
-    string public constant override name     = "pBoneShards";
-    string public constant override symbol   = "pBONESHARDS";
-    uint8  public constant override decimals = 18;
+contract BoneShardsPoly is ERC20 {
+	string public constant override name = "pBoneShards";
+	string public constant override symbol = "pBONESHARDS";
+	uint8 public constant override decimals = 18;
 }
